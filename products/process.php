6d71c375b3e67d9047e92df47fa616e1faeb0e91
emilyfoxhu <alismail@foxmail.com>
2020-11-20 14:01:48 -0800

    top 5 rating

--- products/process.php
+++ products/process.php
@@ -6,35 +6,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>BreadnSweets</title>
     <link href="../css/style.css" rel="stylesheet" type="text/css">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <!--The following script tag downloads a font from the Adobe Edge Web Fonts server for use within the web page. We recommend that you do not modify it.--><script>var __adobewebfontsappname__="dreamweaver"</script><script src="http://use.edgefonts.net/brush-script-std:n4:default;unifrakturcook:n7:default;devonshire:n4:default.js" type="text/javascript"></script>
+    <script>var __adobewebfontsappname__="dreamweaver"</script><script src="http://use.edgefonts.net/brush-script-std:n4:default;unifrakturcook:n7:default;devonshire:n4:default.js" type="text/javascript"></script>
 </head>
-
 <body>
 <div class="container">
     <header>
         <div class="primary_header">
-            <footer><em><a href="lab_in_class/index.php">&nbsp; Lab in Class</a></em></footer>
+            <footer><em><a href="../lab_in_class/index.php">&nbsp; Lab in Class</a></em></footer>
             <h1 class="title"> Bread &amp; Sweets</h1>
         </div>
         <nav class="secondary_header" id="menu">
             <ul>
-                <li><a href="index.php">HOME</a></li>
-                <li><a href="about.html">ABOUT</a></li>
-                <li><a href="products.html">PRODUCTS</a></li>
-                <li><a href="services.html">SERVICES</a></li>
-                <li><a href="news.html">NEWS</a></li>
-                <li><a href="contact.php">CONTACTS</a></li>
-                <li><a href="user.html">USER</a></li>
+                <li><a href="../index.php">HOME</a></li>
+                <li><a href="../about.html">ABOUT</a></li>
+                <li><a href="../products.html">PRODUCTS</a></li>
+                <li><a href="../services.html">SERVICES</a></li>
+                <li><a href="../news.html">NEWS</a></li>
+                <li><a href="../contact.php">CONTACTS</a></li>
+                <li><a href="../user.html">USER</a></li>
             </ul>
         </nav>
     </header>
     <section>
+        <h2 class="noDisplay">Main Content</h2>
         <article class="left_article">
             <h3>&nbsp;</h3>
             <p>Thanks for your review!</p>
@@ -102,16 +96,14 @@
         </div>
     </div>
     <div class="social">
-        <p class="social_icon"><img src="images/foot1.jpg" alt="" width="100" height="0" class="thumbnail"/></p>
-        <p class="social_icon"><img src="images/foot2.jpg" width="204" alt="" class="thumbnail"/></p>
-        <p class="social_icon"><img src="images/foot3.jpg" width="900" alt="" class="thumbnail"/></p>
-        <p class="social_icon"><img src="images/foot4.jpg" width="1137" alt="" class="thumbnail"/></p>
+        <p class="social_icon"><img src="../images/foot1.jpg" alt="" width="100" height="0" class="thumbnail"/></p>
+        <p class="social_icon"><img src="../images/foot2.jpg" width="204" alt="" class="thumbnail"/></p>
+        <p class="social_icon"><img src="../images/foot3.jpg" width="900" alt="" class="thumbnail"/></p>
+        <p class="social_icon"><img src="../images/foot4.jpg" width="1137" alt="" class="thumbnail"/></p>
     </div>
     <footer class="secondary_header footer">
         <div class="copyright">&copy;2020 - BREAD&amp;SWEETS WEBPAGE&nbsp; <strong>&nbsp;</strong></div>
     </footer>
 </div>
 </body>
-</head>
 </html>
-
