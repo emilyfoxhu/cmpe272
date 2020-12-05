fe8a024416fda38eea1201ca27f3da360395f49b
Yihu <alismail@foxmail.com>
2020-12-04 21:32:44 -0800

    delete product track from own company

--- products.php
+++ products.php
@@ -39,22 +39,22 @@
       <h3>products</h3>
         <?php
 
-        // Include the history functionality
-        include_once('history.php');
-
-        // Displaying the contents of history for checking
-        $ct = 0;
-        foreach($_SESSION['pageadd'] as $pageadd)
-        {
-            $ct = $ct+1;
-            echo 'Page History Entry #'.$ct.' of '.
-                count($_SESSION['pageadd']).' is '.$pageadd.'<br>';
-        }
-        echo '<br>';
-
-        // Include a page to allow destroying the session
-        // (for code check purpose)
-        echo "<a href=destroy.php>Destroy Session</a>";
+//        // Include the history functionality
+//        include_once('history.php');
+//
+//        // Displaying the contents of history for checking
+//        $ct = 0;
+//        foreach($_SESSION['pageadd'] as $pageadd)
+//        {
+//            $ct = $ct+1;
+//            echo 'Page History Entry #'.$ct.' of '.
+//                count($_SESSION['pageadd']).' is '.$pageadd.'<br>';
+//        }
+//        echo '<br>';
+//
+//        // Include a page to allow destroying the session
+//        // (for code check purpose)
+//        echo "<a href=destroy.php>Destroy Session</a>";
         ?>
     <strong></strong>    </article>
     <div class="row">
