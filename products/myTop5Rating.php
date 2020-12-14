a157a7b411ea94b1c34d040e6fd17562d522e57d
Yihu <alismail@foxmail.com>
2020-12-13 17:12:48 -0800

    user top 5 rating

--- products/myTop5Rating.php
+++ products/myTop5Rating.php
@@ -1,3 +1,6 @@
+<table border="5" cellpadding="0" cellspacing="2">
+    <tr><td>Product Name</td>
+        <td'>Average Rating</td></tr>
 <?php
     $servername="us-cdbr-east-02.cleardb.com";
     $username="b4f09a430a2ca0";
@@ -12,9 +15,12 @@
     for ($counter = 0; $row = mysqli_fetch_row($result); $counter++){
 
         foreach($row as $key => $value){
-            print("$value ");
-            print("\n");
+            print("<tr>");
+            foreach($row as $key => $value)
+                print("<td>$value</td>");
+            print("</tr>");
         }
     }
     mysqli_close($conn);
 ?>
+</table>
